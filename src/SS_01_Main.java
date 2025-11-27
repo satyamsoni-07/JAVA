dff26db7a786680f5f8741cb41b4c188e75fd01d
satyamsoni-07 <satyamsoni07sep@gmail.com>
2025-11-27 07:18:46 +0530

    new topic primitive data type

---
+++ src/SS_01_Main.java
@@ -0,0 +1,12 @@
+class SS_01_Main {
+	public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // Q:- get an output of sum of 3 numbers //
+        int num1=5;
+        int num2=10;
+        int num3=15;
+        System.out.println(num1+num2+num3);
+
+    }
+}
---
+++ src/SS_02_Literals.java
@@ -0,0 +1,23 @@
+public class SS_02_Literals {
+    public static void main(String[] args) {
+        byte age = 69;
+        int age2 = 15;
+        short age3 = 28;
+        long ageDino = 141365365L;
+        char ch = 'S';
+        float f1 = 8.5f;
+        double d1 = 5.18;
+        boolean a =  true;
+
+        System.out.println(age);
+        System.out.println(age2);
+        System.out.println(age3);
+        System.out.println(ageDino);
+        System.out.println(ch);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(a);
+
+
+    }
+}
