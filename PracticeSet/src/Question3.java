ee009b30a6deaf387dabb64dd62cbc803be6a487
satyamsoni-07 <satyamsoni07sep@gmail.com>
2025-11-29 23:56:34 +0530

    questions practice

--- PracticeSet/src/Question3.java
+++ PracticeSet/src/Question3.java
@@ -2,22 +2,34 @@
 Write a program to calculate CGPA using marks of three subjects (out of 100 marks each)
  */
 
+import java.util.Scanner;
+
 public class Question3 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        System.out.println("sum of three number:");
-        int num1 = 15;
-        int num2 = 46;
-        int num3 = 43;
-        int sum = (num1 + num2 + num3);
+        System.out.println("Taking Input From the user");
+        Scanner sc =  new Scanner(System.in);
 
-        System.out.println(sum);
+        System.out.println("Subject 1");
+        int a = sc.nextInt();
+
+        System.out.println("Subject 2");
+        int b = sc.nextInt();
+
+        System.out.println("Subject 3");
+        int c = sc.nextInt();
+
+        int sum = (a +b +c);
+
+        float CGPA = sum/30.0f;
+        System.out.println("CGPA of your semester is:");
+        System.out.println(CGPA);
+
+
+
+
 
 
     }
-
-
 }
-
-
--- PracticeSet/src/Question4.java
+++ PracticeSet/src/Question4.java
@@ -8,13 +8,8 @@ public class Question4 {
 
     public static void main(String[] args){
 
-        System.out.println("sum of three number:");
-        int num1 = 15;
-        int num2 = 46;
-        int num3 = 43;
-        int sum = (num1 + num2 +num3);
 
-        System.out.println(sum);
+
 
 
     }
