e5e8e584bb4a4cbcd5e27dc1e0a2f755936eed35
satyamsoni-07 <satyamsoni07sep@gmail.com>
2025-12-01 16:27:53 +0530

    questions practice

--- PracticeSet/src/Question4.java
+++ PracticeSet/src/Question4.java
@@ -3,14 +3,18 @@ Write a Java program which asks the user to enter his/her name and greet them wi
 ("Hello<name>, have a Good day")
  */
 
+import java.util.Scanner;
+
 public class Question4 {
 
 
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("What's your Name");
+        String name = sc.next();
 
-
-
+        System.out.println("Hello " +name +" I Love You");
 
     }
 
--- PracticeSet/src/Question5.java
+++ PracticeSet/src/Question5.java
@@ -2,18 +2,19 @@
 Write a Java program to Convert Kilometer to miles
  */
 
+import java.util.Scanner;
+
 public class Question5 {
-
-
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("sum of three number:");
-        int num1 = 15;
-        int num2 = 46;
-        int num3 = 43;
-        int sum = (num1 + num2 +num3);
 
-        System.out.println(sum);
+        System.out.println("Enter Kilometer");
+        int Kilometer = sc.nextInt();
+
+        float miles = Kilometer * 0.621371f;;
+        System.out.println(Kilometer +"kilometer = " +miles +"miles");
+
 
 
     }
