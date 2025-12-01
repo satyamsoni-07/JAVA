5b31e12064122b38331d965c470dc3e54c61c291
satyamsoni-07 <satyamsoni07sep@gmail.com>
2025-12-01 21:31:11 +0530

    questions practice

--- PracticeSet/src/Question6.java
+++ PracticeSet/src/Question6.java
@@ -1,20 +1,15 @@
 /*
 Write a Java program to detect whether a number entered by the user is integer or not
  */
+import java.util.Scanner;
 
 public class Question6 {
-
-
-
     public static void main(String[] args){
 
-        System.out.println("sum of three number:");
-        int num1 = 15;
-        int num2 = 46;
-        int num3 = 43;
-        int sum = (num1 + num2 +num3);
+        System.out.print("Enter Anything: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.print(sc.hasNextInt());
 
-        System.out.println(sum);
 
 
     }
