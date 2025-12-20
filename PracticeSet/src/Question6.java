2536e03da020e7c77e71534b838c8e08c5a65d42
satyamsoni-07 <satyamsoni07sep@gmail.com>
2025-12-20 21:14:53 +0530

    questions practice

--- PracticeSet/src/Question6.java
+++ PracticeSet/src/Question6.java
@@ -1,15 +1,15 @@
 /*
 Write a Java program to detect whether a number entered by the user is integer or not
  */
+
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Question6 {
-    public static void main(String[] args){
-
-        System.out.print("Enter Anything: ");
+    public static void main(String[] args) {
+        System.out.println("Enter Your Number");
         Scanner sc = new Scanner(System.in);
-        System.out.print(sc.hasNextInt());
-
+        System.out.println(sc.hasNextInt());
 
 
     }
