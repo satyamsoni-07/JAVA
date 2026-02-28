2e146fbdb36df860f560065f231207ee03081f98
satyamsoni-07 <satyamsoni07sep@gmail.com>
2026-02-28 13:21:02 +0530

    new code

--- src/J1Main.java
+++ src/J1Main.java
@@ -1,8 +1,8 @@
-public class Main {
+public class J1Main {
 
     public static void main(String[] args){
         System.out.println("I Like Pizza");
-        System.out.println("I ts vey good");
+        System.out.println("It's very good");
 
 
     }
---
+++ src/J2Variables.java
@@ -0,0 +1,23 @@
+public class J2Variables {
+
+    public static void main (String[] args){
+
+        //FOR INT
+        int age = 20;
+        int year = 2005;
+
+        System.out.println("The Year Is "+year);
+        System.out.println("The Age Is "+age);
+
+
+        //FOR DOUBLE
+        double price = 50.99;
+        double cgpa = 9.5;
+        double temperature = 97.8;
+
+        System.out.println("The price is "+price);
+        System.out.println("The cgpa is "+cgpa);
+        System.out.println("The temperature is "+temperature);
+
+    }
+}
